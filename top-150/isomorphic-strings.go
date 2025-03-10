3ff73f4c53e770b0855d705453f4936aac7c6e2e
Oshan Wisumperuma <warunaoshan@gmail.com>
2025-03-10 12:18:05 +1100

    Update isomorphic-strings.go

--- top-150/isomorphic-strings.go
+++ top-150/isomorphic-strings.go
@@ -1,1 +1,24 @@
 
+
+func isIsomorphic(s string, t string) bool {
+	char_map := make(map[rune]rune)
+	trune := []rune(t)
+	for index, char := range s {
+		val, exists := char_map[char]
+		if !exists {
+			for _, rch := range char_map {
+				if rch == trune[index] {
+					return false
+				}
+			}
+			char_map[char] = trune[index]
+			continue
+		}
+		if val == trune[index] {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
