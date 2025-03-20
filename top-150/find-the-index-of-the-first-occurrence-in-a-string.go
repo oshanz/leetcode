8da90d60cc19acb3f36e6739433165a34860d19f
Oshan Wisumperuma <warunaoshan@gmail.com>
2025-03-21 09:27:00 +1100

    Update find-the-index-of-the-first-occurrence-in-a-string.go

--- top-150/find-the-index-of-the-first-occurrence-in-a-string.go
+++ top-150/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -3,15 +3,15 @@ func strStr(haystack string, needle string) int {
 	haystackrunes := []rune(haystack)
 	needleLen := len(needle)
 	haystackLen := len(haystack)
-    needleFirstChar := []rune(needle)[0]
-   // fmt.Println(needleFirstChar)
+	needleFirstChar := []rune(needle)[0]
+	// fmt.Println(needleFirstChar)
 	for i, char := range haystackrunes {
 		if haystackLen-i < needleLen {
 			return -1
 		}
 		if char != needleFirstChar {
-            continue
-        }
+			continue
+		}
 		// fmt.Println("i ",i, " l ", needleLen)
 		if string(haystackrunes[i:i+needleLen]) == needle {
 			return i
