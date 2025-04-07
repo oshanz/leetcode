ff393416baa613f617dc499ae54aa331fd18abe0
Oshan Wisumperuma <warunaoshan@gmail.com>
2025-04-08 06:48:41 +1000

    Create search-in-a-binary-search-tree.go

---
+++ leetcode-75/search-in-a-binary-search-tree.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+	if node := searchBST(root.Left, val); node != nil {
+		return node
+	}
+	if node := searchBST(root.Right, val); node != nil {
+		return node
+	}
+	return nil
+}
